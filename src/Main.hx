496a703bf14516f786e4ae8d22930d0a05c20b40
Zhang DongCai <zhangdongcai@ZhangtekiMac-mini.local>
2019-12-07 23:21:03 +0800

    init

---
+++ src/Main.hx
@@ -0,0 +1,22 @@
+//import org.apache.pdfbox.io.IOUtils;
+//import limilabs.ftp.client.AuthExtension;
+//import limilabs.ftp.BasicConstant;
+//import com.eduworks.ec.storage;
+//import java.javax.swing.*;
+import android.app.Activity;
+
+
+class Main {
+    static public function main() {
+//        var a = new AuthExtension();
+//        trace('Hello, world 222! ${BasicConstant.op_Equality}');
+
+        trace('Hello, world 222! ');
+        var a: Activity;
+
+    }
+
+    public function say() {
+//        var a = BasicConstant.op_Equality;
+    }
+}
